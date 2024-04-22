5e9843ba776e23194e197f4d4c7c874274120321
Sheraz Akram <sherazakram.pro@gmail.com>
2024-04-22 17:17:31 +0500

    setup express server

--- server.ts
+++ server.ts
@@ -1,1 +1,10 @@
-console.log("welcome to the elibrary server");
+import app from "./src/app";
+
+const startServer = () => {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+  });
+};
+
+startServer();
