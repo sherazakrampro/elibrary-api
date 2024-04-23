b5e55bc7ab92bef7b0e14dd8e2065989b6280763
Sheraz Akram <sherazakram.pro@gmail.com>
2024-04-23 20:52:36 +0500

    jwt token created

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,6 +5,7 @@ const _config = {
   port: process.env.PORT,
   dbUrl: process.env.DB_URL,
   env: process.env.NODE_ENV,
+  jwtSecret: process.env.JWT_SECRET,
 };
 
 export const config = Object.freeze(_config);
--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,9 @@
 import { Request, Response, NextFunction } from "express";
 import createHttpError from "http-errors";
 import User from "../models/userModel";
+import bcrypt from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
 
 // register a user
 const registerUser = async (
@@ -9,21 +12,32 @@ const registerUser = async (
   next: NextFunction
 ) => {
   const { name, email, password } = req.body;
-  // validation
+  // 1. validation
   if (!name || !email || !password) {
     const error = createHttpError(400, "All fields are required");
     return next(error);
   }
-  // check-in database
+  // 2. check-in database
   const user = await User.findOne({ email });
   if (user) {
     const error = createHttpError(400, "User already exists");
     return next(error);
   }
-  // process
-  // response
-  res.json({
-    message: "user registered successfully",
+  // 3. password hashing
+  const hashedPassword = await bcrypt.hash(password, 10);
+  // create user
+  const newUser = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+  // 4. token generation - JWT
+  const token = sign({ _id: newUser._id }, config.jwtSecret as string, {
+    expiresIn: "7d",
+  });
+  // 5. response
+  res.status(201).json({
+    accessToken: token,
   });
 };
 
