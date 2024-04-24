98e47193f86828a24e0906a8e20e3bd2562861c6
Sheraz Akram <sherazakram.pro@gmail.com>
2024-04-24 08:49:28 +0500

    login user controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -62,4 +62,52 @@ const registerUser = async (
   }
 };
 
-export { registerUser };
+// Login a user
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+
+  // 1. validation
+  if (!email || !password) {
+    const error = createHttpError(400, "All fields are required");
+    return next(error);
+  }
+
+  // 2. check if user exists
+  let existingUser: User | null;
+  try {
+    existingUser = await UserModel.findOne({ email });
+    if (!existingUser) {
+      const error = createHttpError(401, "User not found!");
+      return next(error);
+    }
+  } catch (error) {
+    return next(createHttpError(500, "Error while checking for user"));
+  }
+
+  // 3. check if password is correct
+  const isPasswordCorrect = await bcrypt.compare(
+    password,
+    existingUser.password
+  );
+  if (!isPasswordCorrect) {
+    const error = createHttpError(401, "Invalid password");
+    return next(error);
+  }
+
+  // 4. sign a token for the user
+  try {
+    const token = sign({ _id: existingUser._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
+      algorithm: "HS256",
+    });
+
+    // 5. response
+    res.status(201).json({
+      accessToken: token,
+    });
+  } catch (error) {
+    return next(createHttpError(500, "Error while signing token"));
+  }
+};
+
+export { registerUser, loginUser };
--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,9 +1,12 @@
 import express from "express";
-import { registerUser } from "../controllers/userController";
+import { loginUser, registerUser } from "../controllers/userController";
 
 const userRouter = express.Router();
 
 // user registration route
 userRouter.post("/register", registerUser);
+
+// user login route
+userRouter.post("/login", loginUser);
 
 export default userRouter;
