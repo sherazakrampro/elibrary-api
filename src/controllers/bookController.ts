60a86d7248d11f77dc63b2e380bd75e9a5eac4f2
Sheraz Akram <sherazakram.pro@gmail.com>
2024-04-24 16:35:30 +0500

    new book functionality

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -2,29 +2,35 @@ import { Request, Response, NextFunction } from "express";
 import cloudinary from "../config/cloudinary";
 import path from "node:path";
 import createHttpError from "http-errors";
+import BookModel from "../models/bookModel";
+import fs from "node:fs";
 
 // Register a book controller
 const regiterBook = async (req: Request, res: Response, next: NextFunction) => {
+  // destructuring the data
+  const { title, genre } = req.body;
+
+  // types of files for typescript
+  const files = req.files as { [filename: string]: Express.Multer.File[] };
+
+  // 1. UPLOAD COVER IMAGE
+  // get cover image type => by default 'image/jpeg' (split it by '/' and get the last element 'jpeg')
+  const coverImageType = files.coverImage[0].mimetype.split("/").at(-1);
+
+  // get cover image file name
+  const coverImageFileName = files.coverImage[0].filename;
+
+  // get cover image file path
+  const coverImageFilePath = path.resolve(
+    __dirname,
+    "../../public/data/uploads",
+    coverImageFileName
+  );
+
+  // upload
+  let coverImageUploadResult;
   try {
-    // types of files for typescript
-    const files = req.files as { [filename: string]: Express.Multer.File[] };
-
-    // 1. UPLOAD COVER IMAGE
-    // get cover image type => by default 'image/jpeg' (split it by '/' and get the last element 'jpeg')
-    const coverImageType = files.coverImage[0].mimetype.split("/").at(-1);
-
-    // get cover image file name
-    const coverImageFileName = files.coverImage[0].filename;
-
-    // get cover image file path
-    const coverImageFilePath = path.resolve(
-      __dirname,
-      "../../public/data/uploads",
-      coverImageFileName
-    );
-
-    // upload
-    const coverImageUploadResult = await cloudinary.uploader.upload(
+    coverImageUploadResult = await cloudinary.uploader.upload(
       coverImageFilePath,
       {
         filename_override: coverImageFileName,
@@ -32,37 +38,70 @@ const regiterBook = async (req: Request, res: Response, next: NextFunction) => {
         format: coverImageType,
       }
     );
+  } catch (error) {
+    console.log("failed to upload cover image", error);
+    return next(createHttpError(500, "failed to upload cover image"));
+  }
 
-    // 2. UPLOAD BOOK FILE
-    // get book file type => by default 'application/pdf' (split it by '/' and get the last element 'pdf')
-    const bookFileType = files.file[0].mimetype.split("/").at(-1);
+  // 2. UPLOAD BOOK FILE
+  // get book file type => by default 'application/pdf' (split it by '/' and get the last element 'pdf')
+  const bookFileType = files.file[0].mimetype.split("/").at(-1);
 
-    // get book file name
-    const bookFileName = files.file[0].filename;
+  // get book file name
+  const bookFileName = files.file[0].filename;
 
-    // get book file path
-    const bookFilePath = path.resolve(
-      __dirname,
-      "../../public/data/uploads",
-      bookFileName
-    );
+  // get book file path
+  const bookFilePath = path.resolve(
+    __dirname,
+    "../../public/data/uploads",
+    bookFileName
+  );
 
-    // upload
-    const bookUploadResult = await cloudinary.uploader.upload(bookFilePath, {
+  // upload
+  let bookFileUploadResult;
+  try {
+    bookFileUploadResult = await cloudinary.uploader.upload(bookFilePath, {
       resource_type: "raw",
       filename_override: bookFileName,
       folder: "books",
       format: bookFileType,
     });
-
-    // response
-    console.log(coverImageUploadResult);
-    console.log(bookUploadResult);
-    res.json({ message: "Book registered successfully" });
   } catch (error) {
-    console.log("Error while uploading files", error);
-    return next(createHttpError(500, "Error while uploading files"));
+    console.log("failed to upload book file", error);
+    return next(createHttpError(500, "failed to upload book file"));
   }
+
+  // console.log(coverImageUploadResult);
+  // console.log(bookFileUploadResult);
+
+  // create new book
+  let newBook;
+  try {
+    newBook = await BookModel.create({
+      title,
+      genre,
+      author: "6627c2a3458cec770c1da90a",
+      coverImage: coverImageUploadResult.secure_url,
+      file: bookFileUploadResult.secure_url,
+    });
+  } catch (error) {
+    console.log("failed to create new book", error);
+    return next(createHttpError(500, "failed to create new book"));
+  }
+
+  // delete temp files
+  try {
+    await fs.promises.unlink(coverImageFilePath);
+    await fs.promises.unlink(bookFilePath);
+  } catch (error) {
+    console.log("failed to delete temp files", error);
+    return next(createHttpError(500, "failed to delete temp files"));
+  }
+
+  // response
+  res
+    .status(201)
+    .json({ message: "Book registered successfully", id: newBook._id });
 };
 
 export { regiterBook };
